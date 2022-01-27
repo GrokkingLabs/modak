1c7641a4d136b9ced606522635d709a7346447c6
Shakti.Misra <shakti.misra.works@gmail.com>
2022-01-27 12:20:46 +0530

    Adding duckdb source files
    Adding duckdb source files
    - Adding the amalgamation
    - Removing the library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
 #include "utils/Logger.hpp"
+#include <duckdb.hpp>
+#include <iostream>
 
 int main() {
   using namespace grok;
   using namespace std;
+  using namespace duckdb;
   glog.info("Starting the application");
+  DuckDB db(nullptr);
+  Connection con(db);
+  // create a table
+  con.Query("CREATE TABLE integers(i INTEGER, j INTEGER)");
+  // insert three rows into the table
+  con.Query("INSERT INTO integers VALUES (3, 4), (5, 6), (7, NULL)");
+  auto res = con.Query("SELECT * FROM integers");
+  auto& result = *res;
+  if (!result.success) {
+    glog.error("{}", result.error);
+  } else {
+    glog.info("\n{}", result.ToString());
+  }
+
   return 0;
 }
